9a07a37332976453e235ab035c4ca4d26c02d53b
ionutsilviu3 <ionut.boanc@student.unitbv.ro>
2023-01-04 16:30:28 +0200

    Added a Main class to call the Manager "ruleaza" procedure

---
+++ lfc/TemaRegex/Main.java
@@ -0,0 +1,13 @@
+package TemaRegex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Manager manager = new Manager();
+        manager.ruleaza();
+    }
+
+}
